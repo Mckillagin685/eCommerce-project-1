786f33af3339e0ba6b739469f26dcebc9e645fcb
Lucas Kirsch <grimreaper685@gmail.com>
2017-06-30 10:17:04 -0700

    ready for action

--- homepage.js
+++ homepage.js
@@ -20,18 +20,6 @@ $('form').on('submit', (e) => {
     }
 });
 
-function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-}
-
 $('#arrowforward').on('click', (e) => {
   $('.slider').slider('pause');
   $('.slider').slider('next');
