920f4b4c59590dc8a682d86d70af0c1666145737
Lucas Kirsch <grimreaper685@gmail.com>
2017-06-30 09:55:34 -0700

    homepage and checkout

---
+++ checkout.js
@@ -0,0 +1,36 @@
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+$(document).ready(function() {
+  $('select').material_select();
+});
+
+$('form').on('submit', (e) => {
+    Materialize.toast('Order successful', 5000);
+});
+
+$('input[type="checkbox"]').on('click', (e) => {
+  const checkbox = $('input[type="checkbox"]');
+
+  if(checkbox.checked){
+    $('#bill_first_name').val($('#first_name').val())
+    $('#bill_last_name').val($('#last_name').val())
+    $('#bill_company').val($('#company').val())
+    $('#bill_address_1').val($('#address_1').val())
+    $('#bill_address_2').val($('#address_2').val())
+    $('#bill_select').val($('#select').val())
+    $('#bill_zip_code').val($('#zip_code').val())
+    $(document).ready(function() {
+      Materialize.updateTextFields();
+    });
+  }
+})
